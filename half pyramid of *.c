60fdeb1f172882ce216094e2c4708c04e927066f
GaneshRao16 <110719353+GaneshRao16@users.noreply.github.com>
2022-08-28 18:06:28 +0530

    initial commit

---
+++ half pyramid of *.c
@@ -0,0 +1,12 @@
+//printing half pyramid of *
+#include<stdio.h>
+int main()
+{
+    int i,j,row;
+    for(i=1;i<=row;i++);
+    {
+        for(j=1;j<=i;j++);
+        printf("%d",1);
+    }
+    return 0;
+}
