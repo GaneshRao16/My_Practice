53389c2808698b5a14232a2af188f1abc268c4a5
GaneshRao16 <110719353+GaneshRao16@users.noreply.github.com>
2022-08-28 18:34:39 +0530

    Update half pyramid of *.c

--- half pyramid of *.c
+++ half pyramid of *.c
@@ -3,10 +3,15 @@
 int main()
 {
     int i,j,row;
-    for(i=1;i<=row;i++);
+    printf("Enter the number of rows:");
+    scanf("%d",&row);
+    for(i=1;i<=row;i++)
     {
-        for(j=1;j<=i;j++);
-        printf("%d",1);
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
     }
     return 0;
 }
